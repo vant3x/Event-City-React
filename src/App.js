8d2c5b4a40eeea0d84003d55ed034d5c12420138
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-10-04 15:42:01 -0500

    Obteniendo las categorías

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Header from './components/Header';
 import './css/App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=I6SI2ETPDSJGLYQXX4JH&locale=es_ES`
+    
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categorias => {
+        console.log(categorias.categories)
+      })
+  }
+
   render() {
     return (
       <div className="App">
