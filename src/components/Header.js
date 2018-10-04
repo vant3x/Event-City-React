e6f7db5fbe9a9dc8ad1ce5b631580b1d4d6c9c85
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-10-04 14:27:44 -0500

    Inicio app, header, cover y estilos básicos

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className="uk-margin" uk-margin="true"> 
+				<div className="header-img"></div>
+				<h1 className="uk-text-center title">Event City</h1>
+		</header>	
+	);
+};
+
+export default Header;
